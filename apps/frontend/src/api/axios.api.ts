53da8324109207d256988902c856ba6e99421290
HawK <andri0667365973@gmail.com>
2024-05-20 00:09:22 +0300

    f-finish-6

--- apps/frontend/src/api/axios.api.ts
+++ apps/frontend/src/api/axios.api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export const BACKEND_URL = process.env.BACKEND_URL;
+export const BACKEND_URL = 'https://events-registration-app-po2b.onrender.com/';
 
 export const instance = axios.create({ baseURL: BACKEND_URL, withCredentials: true });
